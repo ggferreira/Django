74527444cba4f16abfd985dd7373a8dac9893dad
ggferreira <Gg545800>
2021-03-11 11:54:52 -0300

    SECRET_KEY configurada para diferentes instancias close #7

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nca+n)**gzln)c@(a_7%k088s81fsn*uvra(=l@&3%7q(ft3n!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
