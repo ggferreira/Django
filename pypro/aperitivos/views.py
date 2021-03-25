deafcb9e9a69487b9f14616eed548c1ce44440ec
ggferreira <Gg545800>
2021-03-25 16:08:56 -0300

    Fluxo HTTP no Django - ✓ Herança de Templates - ✓ Bloco de Template - ✓ Contexto de Template - ✓ Video de Acordo com Slug - ✓

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 def video(request, slug):
     videos = {
-        'motivacao':{'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '527255451'},
-        'abertura':{'titulo': 'Abertura do Curso', 'vimeo_id': '528991309'},
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '527255451'},
+        'abertura': {'titulo': 'Abertura do Curso', 'vimeo_id': '528991309'},
     }
-    video= videos[slug]
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
