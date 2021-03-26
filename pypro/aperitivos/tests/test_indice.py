2ecae7913d67b86d0d4b530cfaf462269c97f8c9
ggferreira <Gg545800>
2021-03-26 10:40:11 -0300

    Adicionada pagina 'Aperitivos' com links para todos os videos

---
+++ pypro/aperitivos/tests/test_indice.py
@@ -0,0 +1,35 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:indice'))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+@pytest.mark.parametrize(
+    'titulo',
+    [
+        'Abertura do Curso'
+        'Video Aperitivo: Motivação',
+    ]
+)
+def test_title_video(resp, titulo):
+    assert_contains(resp, titulo)
+
+
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'abertura',
+        'motivacao',
+    ]
+)
+def test_link_video(resp, slug):
+    assert_contains(resp, reverse('aperitivos:video', args=(slug,)))
