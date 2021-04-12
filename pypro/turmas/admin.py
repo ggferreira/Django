8a28522afef331f18d7254c3f31a0679a660a921
ggferreira <Gg545800>
2021-04-12 15:37:12 -0300

    Adcionada funcionalidade de Matriculas

--- pypro/turmas/admin.py
+++ pypro/turmas/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from pypro.turmas.models import Turma
 
 
+class MatriculaInline(admin.TabularInline):
+    model = Turma.alunos.through
+    extra = 1
+    readonly_fields = ('date',)
+    autocomplete_fields = ('usuario',)
+    ordering = ('date',)
+
+
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
+    inlines = [MatriculaInline]
     list_display = ('nome', 'slug', 'inicio', 'fim')
     prepopulated_fields = {'slug': ('nome',)}
     ordering = ('-inicio',)
